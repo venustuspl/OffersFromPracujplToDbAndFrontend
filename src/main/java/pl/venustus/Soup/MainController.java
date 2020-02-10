c2c231ad1c9ab7b7098b3f6ff7099b0752e54e1c
venustuspl <venustus.pl@gmail.com>
2020-02-10 22:03:12 +0100

    1.0.1

--- src/main/java/pl/venustus/Soup/MainController.java
+++ src/main/java/pl/venustus/Soup/MainController.java
@@ -3,12 +3,15 @@ package pl.venustus.Soup;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -25,6 +28,24 @@ public class MainController {
         }
         return null;
     }
+
+    @GetMapping("/api/offers")
+    public List<Element> getOffers() {
+        try {
+            List result = new ArrayList();
+            Document document = Jsoup.connect("https://www.pracuj.pl/praca/junior%20java%20developer;kw/warszawa;wp").get();
+            Elements elements = document.select("a.class.offer-details__title-link");
+            for (Element element : elements){
+                result.add(element);
+            }
+
+            return elements;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 
 }
 
