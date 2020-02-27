d6ddc69764e850d7dc823fb63ee52feb046b553d
venustuspl <venustus.pl@gmail.com>
2020-02-27 22:28:21 +0100

    1.4.8 EmailScheduler

--- src/main/java/pl/venustus/Soup/controller/OfferController.java
+++ src/main/java/pl/venustus/Soup/controller/OfferController.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2020. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 package pl.venustus.Soup.controller;
 
 import org.jsoup.Jsoup;
@@ -20,7 +28,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin("*")
+@CrossOrigin
 @RestController
 public class OfferController {
 
--- src/main/java/pl/venustus/Soup/domain/Offer.java
+++ src/main/java/pl/venustus/Soup/domain/Offer.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2020. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 package pl.venustus.Soup.domain;
 
 import lombok.AllArgsConstructor;
@@ -39,7 +47,6 @@ public class Offer {
     @Column(name = "URL", unique = true)
     private String url;
 
-    @Column(name = "DATETIME")
     private LocalDateTime dateTime;
 
 
